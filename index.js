9f3b1457cb98805f630322dd478e7c32773db565
shaunpersad <shaunpersad@gmail.com>
2018-12-17 19:42:24 -0500

    Codec is working

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const Block = require('./Block');
+const blockFactory = require('./blockFactory');
 
 const content = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque interdum rutrum sodales. Nullam mattis fermentum libero, non volutpat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque interdum rutrum sodales. Nullam mattis fermentum libero, non volutpat.';
+const hashes = {};
+const Block = blockFactory(hashes);
 
 const hash = Block.encode(content);
 const decoded = Block.decode(hash);
 
 console.log({
     equals: content === decoded,
-    hashes: Block.hashes,
+    hashes,
     hash
 });
