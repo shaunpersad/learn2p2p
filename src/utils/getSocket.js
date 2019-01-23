42ba5d0a5091677be69cb78ba8daa64c140201ec
shaunpersad <shaunpersad@gmail.com>
2019-01-23 18:41:26 -0500

    remove socket if exists

--- src/utils/getSocket.js
+++ src/utils/getSocket.js
@@ -2,9 +2,15 @@ const path = require('path');
 
 function getSocket() {
 
-    return process.platform === 'win32'
+    const socket = process.platform === 'win32'
         ? path.join('\\\\?\\pipe', process.cwd(), 'cli-socket')
         : path.resolve(__dirname, '../../data/private/socket/cli-socket');
+
+    try {
+        require('fs').unlinkSync(socket);
+    } catch (err) {
+
+    }
 }
 
 module.exports = getSocket;
