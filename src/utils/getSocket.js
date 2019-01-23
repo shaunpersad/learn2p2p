99b49f950b4fce809a0fe38fa831a97cb34f43df
shaunpersad <shaunpersad@gmail.com>
2019-01-23 18:54:34 -0500

    bad idea

--- src/utils/getSocket.js
+++ src/utils/getSocket.js
@@ -2,15 +2,9 @@ const path = require('path');
 
 function getSocket() {
 
-    const socket = process.platform === 'win32'
+    return process.platform === 'win32'
         ? path.join('\\\\?\\pipe', process.cwd(), 'cli-socket')
         : path.resolve(__dirname, '../../data/private/socket/cli-socket');
-
-    try {
-        require('fs').unlinkSync(socket);
-    } catch (err) {
-
-    }
 }
 
 module.exports = getSocket;
